247a4da3b7623648c19b6501257f21bde437aee0
recchan13 <93rezao@gmail.com>
2019-10-29 14:53:41 +0700

    latihan praktikum

--- Latihan3.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $data = array("lanirne", "aduh", "qifuat", "toda","anevi", "samid", "kifuat");
-    
-    sort($data);
-    foreach($data as $i){
-        echo $i;
-        echo "<br>";
-    }
-?>
