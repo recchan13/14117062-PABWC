3377cbfc88f370a4d8d91c855fee421d4ca7f9e5
recchan13 <93rezao@gmail.com>
2019-12-04 14:14:50 +0700

    belum sama tugas

---
+++ latihan3d.php
@@ -0,0 +1,24 @@
+<?
+    $conn = mysql_connect("localhost","root","");
+    mysql_select_db($conn,"pabwdb");
+    $hasil = mysql_query($conn,"select * from bukutamu");
+    $jumlah = mysql_num_rows($hasil);
+    echo "<center>Daftar Pengunjung</center>";
+    echo "Jumlah pengunjung : $jumlah";
+    $a=1;
+    while($baris = mysql_fetch_array($hasil))
+    {
+        echo "<br>";
+        echo $a;
+        echo "<br>";
+        echo "Nama : ";
+        echo $baris[0];
+        echo "<br>";
+        echo "Email : ";
+        echo $baris[1];
+        echo "<br>";
+        echo "Komentar : ";
+        echo $baris[2];
+        $a++;
+    }
+?>
